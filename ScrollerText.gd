cf4f11fdd05b07a3c7df94a04414bc924752c318
Jim Furness <james.w.furness.1@gmail.com>
2023-11-12 13:32:37 -0500

    color bars scaling

--- ScrollerText.gd
+++ ScrollerText.gd
@@ -2,10 +2,13 @@ extends Control
 
 var team_name: String
 var points: int
+var position_carry: float
+var color: Color
 
-func init(team_name: String, points: int):
+func init(team_name: String, points: int, color: Color):
     self.set_team_name(team_name)
     self.set_points(points)
+    self.set_color(color)
 
 func set_team_name(new_name: String):
     self.team_name = new_name
@@ -14,6 +17,14 @@ func set_team_name(new_name: String):
 func set_points(new_points: int):
     self.points = new_points
     $HBoxContainer/Points.text = str(self.points)
+
+func set_color(color: Color):
+    $ColorBar.color = color
+
+func set_color_bar_size(normalization: int, minimum: float, maximum: float):
+    var proportion: float = max(0.0, self.points/float(normalization))
+    var actual_proportion: float = proportion*(maximum - minimum) + minimum
+    $ColorBar.size.x = self.size.x*actual_proportion
 
 func shift_position(shift: Vector2):
     self.position += shift
--- VisualisationWindow.gd
+++ VisualisationWindow.gd
@@ -2,8 +2,14 @@ extends Window
 
 const BidwarTeam = preload("res://ScrollerText.tscn")
 
-const SCROLL_RATE: float = 10 # In pixels per second.
+const SCROLL_RATE: float = 15 # In pixels per second.
 const SPACING: float = 3
+const COLOR_BAR_MIN: float = 0.0
+const COLOR_BAR_MAX: float = 0.9
+
+const COLOR_SATURATION: float = 0.6
+const COLOR_VALUE: float = 0.8
+const COLOR_HUE_PARTITIONS: int = 10
 
 var upper_offset: float = 0 # Dynamically set by the number and size of scroller entries
 var team_list: Array
@@ -22,12 +28,25 @@ func _process(delta):
         var last = self.team_list[-1]
         if last.position.y + last.size.y + SPACING < 0:
             self.reset_scroller_positions()
+            # Now uniform shift to the bottom of the window.
             for team in self.team_list:
                 team.position.y += self.size.y
 
+        self.update_color_bars()
+
+func update_color_bars():
+    var max_points: int = 0
+    for team in self.team_list:
+        max_points = max(max_points, team.points)
+
+    for team in team_list:
+        team.set_color_bar_size(max_points, COLOR_BAR_MIN, COLOR_BAR_MAX)
+
 func _add_team_to_scroller(team_name: String, points: int):
     var new_team = BidwarTeam.instantiate()
-    new_team.init(team_name, points)
+    var color_hue = float(self.get_children().size() % COLOR_HUE_PARTITIONS)/float(COLOR_HUE_PARTITIONS)
+    var color = Color.from_hsv(color_hue, COLOR_SATURATION, COLOR_VALUE)
+    new_team.init(team_name, points, color)
     self.add_child(new_team)
     self.reset_scroller_positions()
 
@@ -40,6 +59,12 @@ func reset_scroller_positions():
     for i in range(self.team_list.size()):
         team_list[i].position.y = self.upper_offset
         self.upper_offset += team_list[i].size.y + SPACING
+    self.update_color_bars()
 
 func _order_teams(a, b) -> bool:
      return a.points > b.points
+
+func _on_size_changed():
+    for team in self.team_list:
+        team.size.x = self.size.x
+    reset_scroller_positions()
